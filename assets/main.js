be62d111845f8cd6e22bcc9332ce7aa88c6a7dcf
TheeroDay <siteside.base@gmail.com>
2022-09-06 15:11:55 +0700

    add style.css, change index.html and main.js

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,19 @@
-var app = new Vue({
+const content = {
+  judul: 'Nyoba dulu ae!',
+  isi: 'Coba dulu ae!',
+  adalagi: 'Ini juga coba!',
+  active: true
+}
+
+const app = new Vue({
   el: '#app',
-  data: {
-    isi: 'Coba dulu ae!',
-    adalagi: 'Ini juga coba!'
+  data: content,
+  methods: {
+    getname: function() {
+      return 'Wel-come ' + this.judul
+    },
+    judulUbah: function(judulBaru) {
+      this.judul = judulBaru
+    }
   }
 })
